1482a65fc9ade71a68c7a4cd41525884e7e7bc1b
Pedro-aasp <paasp0@gmail.com>
2016-04-10 10:31:49 -0300

    Initial commit

---
+++ app/src/main/java/br/com/checknutconcon/checknutcon/BemVindo.java
@@ -0,0 +1,36 @@
+package br.com.checknutconcon.checknutcon;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+
+public class BemVindo extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btnCadastrar;
+    private Button btnSair;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bem_vindo);
+
+        btnCadastrar = (Button)findViewById(R.id.btnCadastrar);
+        btnSair = (Button)findViewById(R.id.btnSair);
+
+        btnCadastrar.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+        Intent it = new  Intent (this, CheckNutConMain.class);
+        startActivity(it);
+
+
+
+    }
+}
---
+++ app/src/main/java/br/com/checknutconcon/checknutcon/CheckNutConMain.java
@@ -0,0 +1,13 @@
+package br.com.checknutconcon.checknutcon;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckNutConMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.check_nut_con_main);
+    }
+}
