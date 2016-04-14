92782217797204d2c96a00581d8e218cd9ebd525
Pedro-aasp <paasp0@gmail.com>
2016-04-14 20:40:20 -0300

    A tela de "Bem vindo" está configurada falta apenas configurar o comando para fechar a aplicação. A tela de "Cadastro do Nutricionista" também está completa, só falta o comando de "sair" (fechar) da aplicação. A tela principal de menus está configurada com uma idéia básica, não garantindo que continuará assim.  Criei um activity para o "Cadastro do cliente".
     TESTETESTETEST

--- app/src/main/java/br/com/checknutconcon/checknutcon/CheckNutConMain.java
+++ app/src/main/java/br/com/checknutconcon/checknutcon/CheckNutConMain.java
@@ -1,27 +1,60 @@
 package br.com.checknutconcon.checknutcon;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.*;
+import android.widget.*;
 
-public class CheckNutConMain extends AppCompatActivity {
+public class CheckNutConMain extends ActionBarActivity implements View.OnClickListener{
+
+    private Button btnNovoCliente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_nut_con_main);
+
+        btnNovoCliente = (Button)findViewById(R.id.btnNovoCliente);
+        btnNovoCliente.setOnClickListener(this);
     }
 
-    public boolean OnCreateContextMenuListener(Menu menu){
-    getMenuInflater().inflate(R.menu.menu_check_nut_con_main, menu);
-        return true;
+    @Override
+    public void onClick(View v) {
+
+
+        Intent it = new Intent(this, CadastroDoCliente.class);
+        startActivity(it);
+
+    }
+
+    public boolean oCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_check_nut_con_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        switch (item.getItemId()){
+            case R.id.mni_acaoCliente:
+
+                break;
+            case R.id.mni_acaoCheckList:
+
+                break;
+            case R.id.mni_acaoSair:
+
+                break;
+
+        }
         return super.onOptionsItemSelected(item);
-
-
     }
+
+
 }
